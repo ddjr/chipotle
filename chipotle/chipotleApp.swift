68bdc169192b3854f34656ca039a7482daa089b8
David Daly <ddalyjr@gmail.com>
2023-07-25 13:18:26 -0400

    Initial Commit

---
+++ chipotle/chipotleApp.swift
@@ -0,0 +1,17 @@
+//
+//  chipotleApp.swift
+//  chipotle
+//
+//  Created by David Daly on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct chipotleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
