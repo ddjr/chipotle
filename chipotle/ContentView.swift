03c051fbd2bb42774e8bde9ec2d31c8721adc304
David Daly <ddalyjr@gmail.com>
2023-07-25 13:44:48 -0400

    add main view

--- chipotle/ContentView.swift
+++ chipotle/ContentView.swift
@@ -10,12 +10,52 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                VStack {
+                    Image("mainBackground")
+                        .resizable()
+                }
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .frame(width: 80)
+                            .foregroundColor(.red)
+                        Text(" 5-10 \n Mins")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.title3)
+                    }
+                    .rotationEffect(Angle(degrees: -25))
+                    .offset(x: -130, y: -100)
+                    .zIndex(10)
+                    VStack {
+                        Text("Meals For One")
+                            .foregroundColor(.brown)
+                            .bold()
+                            .font(.title)
+                        Text("Steak Bowl, steak Bowl with Tortilla on the Side, Torilla on the side")
+                            .lineLimit(2)
+                        Text("ADD TO BAG")
+                            .padding(.horizontal, 25)
+                            .padding(.vertical, 12)
+                            .background(.brown)
+                            .foregroundColor(.white)
+                            .bold()
+                            .underline()
+                            .cornerRadius(30.0)
+                        Text("VIEW ALL MY ORDERS")
+                            .foregroundColor(.brown)
+                            .underline()
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 20)
+                    .background(.white)
+                    .cornerRadius(10.0)
+                }
+            }
+            
         }
-        .padding()
+        .background( Color(uiColor: .systemGray4))
     }
 }
 
